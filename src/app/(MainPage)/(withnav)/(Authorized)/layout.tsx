9c3a9c2773ca40c90c750fbbdfe1cab2967b02c7
Mahi-rejone <mahierjone@gmail.com>
2026-08-09 03:07:12 +0600

    undo

--- src/app/(MainPage)/(withnav)/(Authorized)/layout.tsx
+++ src/app/(MainPage)/(withnav)/(Authorized)/layout.tsx
@@ -1,13 +1,21 @@
-import AuthGuard from "@/components/AuthGuard";
+export const dynamic = "force-dynamic";
+import UrlRedirect from "@/components/urlRedirect";
+import { cookies } from "next/headers";
 
-export default function AuthorizedLayout({
+export default async function AuthorizedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("accessToken")?.value;
+  console.log(token);
+  if (!token) {
+    return <UrlRedirect url="/login" key={1} />;
+  }
   return (
-    <AuthGuard>
+    <div>
       <main className="min-h-screen">{children}</main>
-    </AuthGuard>
+    </div>
   );
 }
