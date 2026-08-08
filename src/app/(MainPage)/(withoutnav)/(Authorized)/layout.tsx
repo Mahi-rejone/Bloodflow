067489fa1c0e464a3e977fd9c4f3bc710b0d1ca3
sayem111103 <sayemul03@gmail.com>
2026-08-09 02:40:59 +0600

    log added for check

--- src/app/(MainPage)/(withoutnav)/(Authorized)/layout.tsx
+++ src/app/(MainPage)/(withoutnav)/(Authorized)/layout.tsx
@@ -9,7 +9,7 @@ export default async function AuthorizedLayout({
 }>) {
   const cookieStore = await cookies();
   const token = cookieStore.get("accessToken")?.value;
-  console.log(token)
+  console.log("without nav",token)
   if (!token) {
     return <UrlRedirect url="/login" key={1} />;
   }
